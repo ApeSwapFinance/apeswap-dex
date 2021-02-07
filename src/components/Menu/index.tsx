2ae9e44865d4892e3d14ca3c3a8b877c176b7490
ape-swap <ape.swap@hotmail.com>
2021-02-06 23:35:56 -0300

    refactor: Linter tweaks

--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -1,11 +1,9 @@
 import React, { useContext } from 'react'
 import { Menu as UikitMenu, ConnectorId } from '@apeswapfinance/uikit'
 import { useWeb3React } from '@web3-react/core'
-import { allLanguages } from 'constants/localisation/languageCodes'
 import { LanguageContext } from 'hooks/LanguageContext'
 import useTheme from 'hooks/useTheme'
 import useGetPriceData from 'hooks/useGetPriceData'
-import useGetLocalProfile from 'hooks/useGetLocalProfile'
 import { injected, bsc, walletconnect } from 'connectors'
 import links from './config'
 
@@ -15,7 +13,6 @@ const Menu: React.FC = (props) => {
   const { isDark, toggleTheme } = useTheme()
   const priceData = useGetPriceData()
   const cakePriceUsd = priceData ? Number(priceData.prices.Cake) : undefined
-  const profile = useGetLocalProfile()
 
   return (
     <UikitMenu
