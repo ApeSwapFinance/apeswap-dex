a0ed99eeb62cd7c04a06be2c1e5a24252e70a13e
defifofum <defifofum0@gmail.com>
2021-02-09 02:46:16 -0500

    add testnet router

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -4,7 +4,8 @@ import { AbstractConnector } from '@web3-react/abstract-connector'
 // import { bsc, fortmatic, injected, portis, walletconnect, walletlink } from '../connectors'
 import { injected, bsc } from '../connectors'
 // TODO
-export const ROUTER_ADDRESS = '0x05fF2B0DB69458A0750badebc4f9e13aDd608C7F'
+// export const ROUTER_ADDRESS = '0x05fF2B0DB69458A0750badebc4f9e13aDd608C7F'
+export const ROUTER_ADDRESS = '0xb66559861f372d4f7b6E5D1dB8C47C31B55238a1'
 
 // a list of tokens by chain
 type ChainTokenList = {
