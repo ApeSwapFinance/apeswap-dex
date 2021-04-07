373f38a87f5bc23616d09aceac9ba5b5ffbbda35
Ixm 759 <759ixm@gmail.com>
2021-04-07 13:37:14 -0500

    refactor estimated convert dollar. change size rate dollar

--- src/components/swap/EstimatedConvertDollar.tsx
+++ src/components/swap/EstimatedConvertDollar.tsx
@@ -9,50 +9,25 @@ import { useTradeExactOut } from 'hooks/Trades'
 
 const EstimatedPriceDollar = styled.div`
   position: absolute;
-  top: 74px;
-  font-size: 9px;
+  top: 73px;
+  font-size: 13px;
   color: ${({ theme }) => theme.colors.textSubtle};
 `
 const EstimatedSymbol = styled.span`
-  font-size: 12px;
+  font-size: 18px;
   height: 0px;
   display: inline-block;
-  line-height: 0.5;
+  line-height: 0.7;
   vertical-align: middle;
 `
-const CURRENCY_BUSD: any = {
-  decimals: 18,
-  name: 'BUSD Token',
-  symbol: 'BUSD',
-	address: "0xe9e7CEA3DedcA5984780Bafc599bD69ADd087D56",
-	chainId: 56
-}
+const CURRENCY_BUSD_ID = "0xe9e7CEA3DedcA5984780Bafc599bD69ADd087D56";
 
 export default function EstimatedConvertDollar({ currency, typedValue = null }: {currency: any, typedValue: any}) {
   
-  const {
-    v1Trade,
-  } = useDerivedSwapInfo()
-	const outputCurrency = useCurrency("0xe9e7CEA3DedcA5984780Bafc599bD69ADd087D56")
+	const outputCurrency = useCurrency(CURRENCY_BUSD_ID)
 	const parsedAmount = tryParseAmount(typedValue, currency);
 	const parsedAmount2 = tryParseAmount("1", outputCurrency ?? undefined);
-	const bestTradeExactOut = useTradeExactOut(currency, parsedAmount2)
-
-  const v2Trade = bestTradeExactOut
-  const { wrapType } = useWrapCallback(
-    currency,
-    outputCurrency ?? CURRENCY_BUSD,
-    typedValue
-  )
-  
-  const showWrap: boolean = wrapType !== WrapType.NOT_APPLICABLE
-	const toggledVersion = Version.v2;
-	const trade = showWrap
-    ? undefined
-    : {
-        [Version.v1]: v1Trade,
-        [Version.v2]: v2Trade,
-      }[toggledVersion]
+	const trade = useTradeExactOut(currency, parsedAmount2)
 	
 	const parsedAmounts = {
         [Field.INPUT]: trade?.inputAmount || parsedAmount,
