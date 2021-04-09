87fb62e455292094f0dd87eb1b3cd78f2011ec8f
Ixm 759 <759ixm@gmail.com>
2021-04-08 21:42:19 -0500

    Adjustment for the calculation of very low denomination tokens. Code refactoring eliminating variables and functions that are not occupied.

--- src/components/swap/EstimatedConvertDollar.tsx
+++ src/components/swap/EstimatedConvertDollar.tsx
@@ -1,7 +1,5 @@
 import React from 'react'
-import { tryParseAmount, useDerivedSwapInfo } from 'state/swap/hooks'
-import useWrapCallback, { WrapType } from 'hooks/useWrapCallback'
-import { Version } from 'hooks/useToggledVersion'
+import { tryParseAmount } from 'state/swap/hooks'
 import { Field } from 'state/swap/actions'
 import styled from 'styled-components'
 import { useCurrency } from 'hooks/Tokens'
@@ -20,28 +18,24 @@ const EstimatedSymbol = styled.span`
   line-height: 0.7;
   vertical-align: middle;
 `
-const CURRENCY_BUSD_ID = "0xe9e7CEA3DedcA5984780Bafc599bD69ADd087D56";
+const CURRENCY_BUSD_ID = '0xe9e7CEA3DedcA5984780Bafc599bD69ADd087D56'
 
-export default function EstimatedConvertDollar({ currency, typedValue = null }: {currency: any, typedValue: any}) {
+export default function EstimatedConvertDollar({ currency, typedValue = null }: { currency: any; typedValue: any }) {
+  const outputCurrency = useCurrency(CURRENCY_BUSD_ID)
+  const parsedAmount = tryParseAmount(typedValue, currency)
+  const parsedAmount2 = tryParseAmount('1', outputCurrency ?? undefined)
+  const trade = useTradeExactOut(currency, parsedAmount2)
+
+  const parsedAmounts = { [Field.INPUT]: trade?.inputAmount || parsedAmount }
   
-	const outputCurrency = useCurrency(CURRENCY_BUSD_ID)
-	const parsedAmount = tryParseAmount(typedValue, currency);
-	const parsedAmount2 = tryParseAmount("1", outputCurrency ?? undefined);
-	const trade = useTradeExactOut(currency, parsedAmount2)
-	
-	const parsedAmounts = {
-        [Field.INPUT]: trade?.inputAmount || parsedAmount,
-        [Field.OUTPUT]: trade?.outputAmount || parsedAmount2,
-      }
-  const formattedAmounts = {
-    [Field.INPUT]: typedValue,
-    [Field.OUTPUT]: Math.round((typedValue*(parsedAmounts[Field.OUTPUT]?.numerator[1]/parsedAmounts[Field.INPUT]?.numerator[1])*100)) / 100
-  }
-	
+  const value = 1/Number(parsedAmounts[Field.INPUT]?.toExact())
+  const decimals = value < 0.01 ? 1000000000 : 100;
+  const rate: any = Number(typedValue) * (Math.round((value) * decimals) / decimals);
+
   return (
     <EstimatedPriceDollar>
       <EstimatedSymbol>~</EstimatedSymbol>
-      <span>${formattedAmounts[Field.OUTPUT] || 0}</span>
+      <span>${rate || 0}</span>
     </EstimatedPriceDollar>
   )
 }
